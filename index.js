1919b94c7a2dda9018ba94ae58782e33f8bb5dc4
Kartikeya <kartik.kapparwan1@gmail.com>
2021-09-10 23:56:06 +0530

    Goals reducer

--- index.js
+++ index.js
@@ -51,6 +51,19 @@ function todos(state = [], action) {
     }
 }
 
+
+// Goals Reducer
+function goals(state = [], action) {
+    switch (action.type) {
+        case 'ADD_GOAL' :
+            return state.concat([action.goal])
+        case 'REMOVE_GOAL' :
+            return state.filter((goal) => goal.id !== action.id)
+        default :
+            return state
+    }
+}
+
 const store = createStore(todos)
 
 store.subscribe(() => {
