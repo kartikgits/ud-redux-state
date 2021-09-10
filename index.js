d6763a30d7cdb09a3b56456a159ca34c530a9f5e
Kartikeya <kartik.kapparwan1@gmail.com>
2021-09-10 11:42:25 +0530

    Subscribe to state change and unsubscribe

--- index.js
+++ index.js
@@ -7,10 +7,32 @@
 
 function createStore () {
     let state
+    let listeners = []
 
     const getState = () => state
 
+    const subscribe = (listener) => {
+        listeners.push(listener)
+        return () => {
+            listeners = listeners.filter((l) => l !== listener)
+        }
+    }
+
     return {
-        getState
+        getState,
+        subscribe
     }
 }
+
+const store = createStore()
+
+store.subscribe(() => {
+    console.log('The new state is: ', store.getState())
+})
+
+// subscribe returns a function, that can be used to unsubscribe the listener
+const unsubscribe = store.subscribe(() => {
+    console.log('The store changed')
+})
+
+unsubscribe()
